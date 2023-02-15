f21837574427871ed4bbc315469a90aea4b4c550
Pedro B <93920727+pedro-barreto@users.noreply.github.com>
2023-02-14 21:37:06 -0300

    ajuste links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <Link to="/" className="logo">
+      <Link to="/site/" className="logo">
         <img
           src="https://styles.redditmedia.com/t5_quz5p/styles/profileIcon_eq31ro3kejw11.jpg?width=256&height=256&frame=1&auto=webp&crop=256:256,smart&s=7089cc5f089b78916ee824dbae601e9a9e04e031"
           alt="Logo"
