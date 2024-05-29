d62c8c80c8cab2fe4bf2be7c3fdc0ead6a714071
Pedro B. <pedrobarreto@alu.ufc.br>
2024-05-29 20:07:47 -0300

    fix: refatoração do header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../img/logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { CgClose } from "react-icons/cg";
@@ -22,31 +22,33 @@ export default function Header() {
     corpo.className = theme;
   }, [hamb, dark]);
 
-  const linkNav = `max group sm:grid place-items-center transition hover:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500 ${
+  const handleNav = () => {
+    window.scrollTo(0, 0);
+    setHamb(false);
+  };
+
+  const navLink = `max group sm:grid place-items-center transition hover:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500 ${
     hamb ? "grid" : "hidden"
   }`;
 
-  const markRoutes =
-    "border-b border-black dark:border-white group-hover:border-gray-200 duration-300";
-
   return (
     <header
-      className={` w-full max-w-screen-2xl grid fixed top-0 bg-cor1 font-nippo z-10 border-b border-black  2xl:border filter opacity-95 dark:bg-gray-800 dark:border-gray-300 ${
+      className={` w-full max-w-screen-2xl grid fixed top-0 bg-cor1 font-nippo z-50 border-b border-black  2xl:border filter opacity-95 dark:bg-gray-800 dark:border-gray-300 ${
         hamb ? "h-full grid-rows-6" : "h-28 grid-cols-12"
       }`}
     >
       <Link
         to="/"
-        className={`justify-center py-3 h-28 col-span-6 sm:col-span-2 ${
-          hamb ? "hidden" : "flex"
+        className={`flex justify-center py-3 h-28 col-span-6 sm:col-span-2 ${
+          hamb && "hidden"
         }`}
-        onClick={() => window.scrollTo(0, 0)}
+        onClick={handleNav}
       >
         <img
           src={Logo}
           alt="logo"
           className={`h-full object-cover duration-300 ${
-            dark ? "" : "filter invert"
+            !dark && "filter invert"
           }`}
         />
       </Link>
@@ -56,99 +58,43 @@ export default function Header() {
         }`}
       >
         <button
-          id="hamburguer"
-          className={`max grid place-items-center text-6xl sm:hidden cursor-pointer ${
-            hamb
-              ? "text-red-500 hover:bg-red-500 hover:text-gray-100 dark:text-red-700 dark:hover:bg-red-700 dark:hover:text-gray-300"
-              : "hover:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500"
+          className={`max grid place-items-center text-6xl sm:hidden cursor-pointer hover:text-gray-200 ${
+            hamb ? "text-red-500 hover:bg-red-500" : " hover:bg-gray-400"
           }`}
-          onClick={() => handleClick()}
+          onClick={handleClick}
         >
-          {hamb ? <CgClose className="text-8xl" /> : <GiHamburgerMenu />}
+          {hamb ? <CgClose /> : <GiHamburgerMenu />}
         </button>
-        <Link
-          to="/"
-          className={linkNav}
-          onClick={() => {
-            setHamb(false);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <Routes>
-            <Route
-              path="/"
-              element={<span className={markRoutes}>HOME</span>}
-            ></Route>
-            <Route path="*" element="HOME"></Route>
-          </Routes>
-        </Link>
 
-        <Link
-          to="/obras"
-          className={linkNav}
-          onClick={() => {
-            setHamb(false);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <Routes>
-            <Route
-              path="/obras"
-              element={<span className={markRoutes}>OBRAS</span>}
-            ></Route>
-            <Route path="*" element="OBRAS"></Route>
-          </Routes>
-        </Link>
+        <NavLink to="/" className={navLink} onClick={handleNav}>
+          <span>HOME</span>
+        </NavLink>
+        <NavLink to="/obras" className={navLink} onClick={handleNav}>
+          <span>OBRAS</span>
+        </NavLink>
+        <NavLink to="/artes" className={navLink} onClick={handleNav}>
+          <span>ARTES</span>
+        </NavLink>
+        <NavLink to="/sobre" className={navLink} onClick={handleNav}>
+          <span>SOBRE</span>
+        </NavLink>
 
-        <Link
-          to="/artes"
-          className={linkNav}
-          onClick={() => {
-            setHamb(false);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <Routes>
-            <Route
-              path="/artes"
-              element={<span className={markRoutes}>ARTES</span>}
-            ></Route>
-            <Route path="*" element="ARTES"></Route>
-          </Routes>
-        </Link>
-
-        <Link
-          to="/sobre"
-          className={linkNav}
-          onClick={() => {
-            setHamb(false);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <Routes>
-            <Route
-              path="/sobre"
-              element={<span className={markRoutes}>SOBRE</span>}
-            ></Route>
-            <Route path="*" element="SOBRE"></Route>
-          </Routes>
-        </Link>
         <button
-          className={`max flex items-center hover:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500 transition duration-300 cursor-pointer p-1 ${
-            hamb ? "" : "hidden"
-          } sm:flex`}
+          className={`max flex sm:flex items-center hover:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-500 transition duration-300 cursor-pointer p-1 ${
+            !hamb && "hidden"
+          } `}
           onClick={() => setDark(!dark)}
         >
           <div className="flex m-auto items-center space-x-2 text-3xl">
             {dark ? (
               <>
                 <BsFillSunFill className="text-yellow-200" />
-                <p>Light</p>
+                <span>Light</span>
               </>
             ) : (
               <>
                 <BsFillMoonFill />
-                <p>Dark</p>
+                <span>Dark</span>
               </>
             )}
           </div>
